6ed51ea3cde2c8e8d18f65c94db04b3fc38c6ea3
Ulyses <ulyses.rico.2012@gmail.com>
2021-05-12 12:04:12 -0500

    ...

--- playWithData.py
+++ playWithData.py
@@ -9,20 +9,19 @@ from InternalControl import cInternalControl
 
 objControl= cInternalControl()
 
- def main():
-    #Get cassandra columns
-    keyspace=objControl.keyspace 
-    table=objControl.table
-    columns_list=[]
-    query="select id from "+keyspace+"."+table+" where secuencia=0 ALLOW FILTERING;"
-    #column_list can be used to name the columns in dataframe
-    bd.getTotalOfRecords(query) 
-    #Fecth any data you want here...the query   
+
+#Get cassandra columns
+keyspace=objControl.keyspace 
+table=objControl.table
+columns_list=[]
+query="select id from "+keyspace+"."+table+" where secuencia=0 ALLOW FILTERING;"
+#column_list can be used to name the columns in dataframe
+bd.getTotalOfRecords(query) 
+#Fecth any data you want here...the query   
     
 
 
 
-if __name__=='__main__':
-    main()    
+  
    
 
